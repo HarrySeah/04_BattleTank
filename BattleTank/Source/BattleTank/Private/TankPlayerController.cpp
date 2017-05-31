f0e7924bc973f993ef3908e20354f1128260206b
HARRYSEAH\Harry <harry_seah@hotmail.com>
2017-05-31 18:08:24 +0800

    Section 4, Lecture 124: Create OUT parameter function

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -16,7 +16,6 @@ void ATankPlayerController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *ControlledTank->GetName());
 	}
-
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
@@ -37,8 +36,19 @@ void ATankPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
 
-	// Get World location if linetrace through crosshair
-	// If it hits the landscape
-		// Tell controlled tank to aim at this point
+	FVector HitLocation; // Out Parameter
+	if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString())
+		
+		// TODO Tell controlled tank to aim at this point
+	}
+}
+
+// Get World location of linetrace through crosshair, true If hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector &OutHitLocation) const
+{
+	OutHitLocation = FVector(1.0);
+	return true;
 }
 
