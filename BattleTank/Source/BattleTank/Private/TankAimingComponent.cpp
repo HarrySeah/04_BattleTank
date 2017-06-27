c50aa4233b9e225f54e6df178b37c0ce6339ed18
HARRYSEAH\Harry <harry_seah@hotmail.com>
2017-06-27 23:04:43 +0800

    Section 4, Lecture 145: Create Fire method with action, create projectile c++ and BP
    Remove some UE_LOG
    
    Create a c++ Class Projectile inherit with actor
    
    Add a method Fire and move UE_LOG inside
    
    Setup Input action in BP Tank

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -56,13 +56,6 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowars(AimDirection);
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
-	}
-	else
-	{
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: No aim solve found"), Time);
 	}
 	// if not solution found do nothing
 
