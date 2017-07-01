f8d8e79ff4c17d475181660e03ee19232c79855e
HARRYSEAH\Harry <harry_seah@hotmail.com>
2017-07-02 00:33:59 +0800

    Section 4, Lecture 180: Programmatic Sideway Fraction
    Reduce the sideway (slipping) problem
    
    GetRightVector() -- > right track X axis
    
    GetComponentVelocity() --> get Tank X

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -3,6 +3,28 @@
 #include "TankTrack.h"
 #include "BattleTank.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
+	// Work-out the required acceleration this frame to correct
+
+	auto CorrectionAcceleration = - SlippageSpeed / DeltaTime * GetRightVector();
+
+	// Calculate and apply sideways for (F = m a)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CoorectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Because 2 tracks
+	TankRoot->AddForce(CoorectionForce);
+}
+
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
