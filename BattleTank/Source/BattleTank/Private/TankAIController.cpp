9ecbb9d696b7d257e5afd65e84960d8264cfe835
HARRYSEAH\Harry <harry_seah@hotmail.com>
2017-07-02 22:53:03 +0800

    Section 4, Lecture 196: Observer Pattern, Use Delegate class
    Create DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTankDelegate); above UCLASS
    
    FTankDelegate become type and declare OnDeath as
    member
    
    member call Broadcast() --- > this will call listener
    
    Listener -- UFUNCTION() --- > OnPossedTankDeath
    
    AddUniqueDynamic ---> register

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -3,6 +3,7 @@
 #include "TankAIController.h"
 #include "BattleTank.h"
 #include "TankAimingComponent.h"
+#include "Tank.h" // so we can impliment OnDeath
 // Depends on movement component via pathfinding system
 
 void ATankAIController::BeginPlay()
@@ -10,6 +11,25 @@ void ATankAIController::BeginPlay()
 	Super::BeginPlay();
 
 }
+
+void ATankAIController::SetPawn(APawn* InPawn)
+{
+	Super::SetPawn(InPawn);
+	if (InPawn)
+	{
+		auto PossessedTank = Cast<ATank>(InPawn);
+		if (!ensure(PossessedTank)) { return; }
+
+		// Subscribe our local method to the tank's death event
+		PossessedTank->OnDeath.AddUniqueDynamic(this, &ATankAIController::OnPossedTankDeath);
+	}
+}
+
+void ATankAIController::OnPossedTankDeath()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Received!"))
+}
+
 
 // Called every frame
 void ATankAIController::Tick(float DeltaTime)
