562f9d525bea2764fd925a7601b80b24ffd1fcc5
HARRYSEAH\Harry <harry_seah@hotmail.com>
2017-07-02 11:13:04 +0800

    Section 4, Lecture 184: Tank AI be better
    Tank AI will fire in certain distance of range and start fire if locked
    
    Create BP class from Tank AI Controller

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -14,13 +14,16 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 
+protected:
+	// How cloase can the AI tank get
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float AcceptanceRadius = 8000;
+
 private:
 	virtual void BeginPlay() override;
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// How cloase can the AI tank get
-	float AcceptanceRadius = 3000;
 
 };
